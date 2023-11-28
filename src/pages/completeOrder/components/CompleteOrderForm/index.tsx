df7c219612d837d0216a8964c33e9a4aaf2d76dc
joaoramosty <devjoaoramos@gmail.com>
2023-11-28 17:33:08 -0300

    Complete Order Page 25%

--- src/pages/completeOrder/components/CompleteOrderForm/index.tsx
+++ src/pages/completeOrder/components/CompleteOrderForm/index.tsx
@@ -1,12 +1,26 @@
+import { useTheme } from "styled-components";
 import { TitleText } from "../../../../components/Typography";
-import { CompleteOrderFormContainer } from "./styles";
+import { SectionTitle } from "../SectionTitle";
+import { CompleteOrderFormContainer, FormSectionContainer } from "./styles";
+import {MapPinLine} from 'phosphor-react'
+import { AddressForm } from "./AddressForm";
 
 export function CompleteOrderForm(){
+    const { colors } = useTheme() ;
+
     return(
         <CompleteOrderFormContainer>
             <TitleText size="xs" color="subtitle" >
                 Complete seu pedido
             </TitleText>
+            <FormSectionContainer>
+                <SectionTitle
+                title="Endereço de entrega"
+                subtitle="Informe o endereço onde deseja receber seu pedido"
+                icon={<MapPinLine size={22} color={colors["brand-yellow-dark"]}/>}
+                />
+                <AddressForm/>
+            </FormSectionContainer>
         </CompleteOrderFormContainer>
     )
 }
--- src/pages/completeOrder/styles.ts
+++ src/pages/completeOrder/styles.ts
@@ -7,3 +7,12 @@ export const CompleteOrderContainer = styled.form`
     gap: 2rem;
 
 `;
+
+export const SectionBaseStyle = styled.div`
+    width: 100%;
+    background: ${({theme})=>theme.colors["base-card"] };
+    border-radius: 6px;
+    padding: 2.5rem;
+    
+  
+`;
