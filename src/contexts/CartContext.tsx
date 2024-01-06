5deddd93c18d87bda0c4e19849ff1ff5fccbdeff
joaoramosty <devjoaoramos@gmail.com>
2024-01-05 23:59:33 -0300

    quantify

--- src/contexts/CartContext.tsx
+++ src/contexts/CartContext.tsx
@@ -2,7 +2,7 @@ import { createContext } from "react";
 import { Coffee } from "../pages/Home/components/CoffeeCard";
 
 export interface CartItem extends Coffee {
-
+    quantify: number;
 }
 
 interface CartContextType{
