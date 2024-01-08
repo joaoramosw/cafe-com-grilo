9e91e5152a386b336b8255dfae0fb81fa46584c3
joaoramosty <devjoaoramos@gmail.com>
2024-01-07 23:55:20 -0300

    add CartItems Usestate

--- src/contexts/CartContext.tsx
+++ src/contexts/CartContext.tsx
@@ -1,4 +1,4 @@
-import { createContext } from "react";
+import { createContext, useState } from "react";
 import { Coffee } from "../pages/Home/components/CoffeeCard";
 
 export interface CartItem extends Coffee {
@@ -10,3 +10,12 @@ interface CartContextType{
 }
 
 export const CartContext = createContext({} as CartContextType)
+
+export function CartContextProvider(){
+    const [CartItems, setCartItems] = useState<CartItem[]>([])
+    return(
+        <CartContext.Provider value={{CartItems}}>
+
+        </CartContext.Provider>
+    )
+}
