0c6b79099ddddc09c783944e93fa51f6e86d6850
joaoramosty <devjoaoramos@gmail.com>
2024-01-09 23:58:51 -0300

    base Provider

--- src/contexts/CartContext.tsx
+++ src/contexts/CartContext.tsx
@@ -1,5 +1,6 @@
-import { createContext, useState } from "react";
+import { ReactNode, createContext, useState } from "react";
 import { Coffee } from "../pages/Home/components/CoffeeCard";
+import { CartContextProvider } from './CartContext';
 
 export interface CartItem extends Coffee {
     quantify: number;
@@ -9,9 +10,13 @@ interface CartContextType{
     CartItems: CartItem[];
 }
 
+interface CartContextProviderProps{
+    children: ReactNode 
+}
+
 export const CartContext = createContext({} as CartContextType)
 
-export function CartContextProvider(){
+export function CartContextProvider({children}:CartContextProviderProps){
     const [CartItems, setCartItems] = useState<CartItem[]>([])
     return(
         <CartContext.Provider value={{CartItems}}>
