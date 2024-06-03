313945862443007f8519c0d95b678e2f5e2ceb30
joaoramosty <devjoaoramos@gmail.com>
2024-06-02 22:58:38 -0300

    chore: Refactor useCart hook to optimize code structure and import statements

--- src/hooks/useCart.ts
+++ src/hooks/useCart.ts
@@ -1,15 +1,5 @@
-import { useContext } from "react";
-import { CartItem } from './../contexts/CartContext';
-import { HomeContainer } from './../pages/home/styles';
-import { Intro } from './../pages/home/components/Intro/index';
-import { OurCoffes } from "../pages/home/components/OurCoffes";
+
 
 export function useCart(){
   const {CartItem} = useCart();
-  return(
-    <HomeContainer>
-      <Intro/>
-      <OurCoffes/>
-    </HomeContainer>
-  )
 }
